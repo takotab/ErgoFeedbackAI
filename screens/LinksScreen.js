9f82f9af6f0d02497fb97fe3c972fb8772f74ffe
tako_desktop <takotabak@gmail.com>
2019-04-25 16:20:13 +0200

    made a far nicer page

--- screens/LinksScreen.js
+++ screens/LinksScreen.js
@@ -1,18 +1,58 @@
 import React from 'react';
-import { ScrollView, StyleSheet } from 'react-native';
-import { ExpoLinksView } from '@expo/samples';
+import { Button, ScrollView, StyleSheet, View, Text } from 'react-native';
+import { ExpoLinksView, Permissions, ImagePicker } from 'expo';
 
 export default class LinksScreen extends React.Component {
   static navigationOptions = {
     title: 'Links',
   };
 
+  _askPermission = async (type, failureMessage) => {
+    const { status, permissions } = await Permissions.askAsync(type);
+
+    if (status === 'denied') {
+      alert(failureMessage);
+    }
+  };
+
+  _takePhoto = async () => {
+    await this._askPermission(Permissions.CAMERA, 'We need the camera permission to take a picture...');
+    await this._askPermission(Permissions.CAMERA_ROLL, 'We need the camera-roll permission to read pictures from your phone...');
+    let pickerResult = await ImagePicker.launchCameraAsync({
+      allowsEditing: true,
+      aspect: [4, 3],
+    });
+
+    this._handleImagePicked(pickerResult);
+  };
+
+  _pickPhoto = async () => {
+    // await this._askPermission(Permissions.CAMERA, 'We need the camera permission to take a picture...');
+    await this._askPermission(Permissions.CAMERA_ROLL, 'We need the camera-roll permission to read pictures from your phone...');
+    let pickerResult = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      aspect: [4, 3],
+    });
+
+    this._handleImagePicked(pickerResult);
+  };
+
   render() {
     return (
       <ScrollView style={styles.container}>
-        {/* Go ahead and delete ExpoLinksView and replace it with your
-           * content, we just wanted to provide you with some helpful links */}
-        <ExpoLinksView />
+        <View><Text>Test</Text></View>
+        <View style={{ marginVertical: 8 }}>
+          <Button
+            onPress={this._takePhoto}
+            title="Take a photo"
+          />
+        </View>
+        <View>
+          <Button
+            onPress={this._pickPhoto}
+            title="Pick Photo"
+          />
+        </View>
       </ScrollView>
     );
   }
