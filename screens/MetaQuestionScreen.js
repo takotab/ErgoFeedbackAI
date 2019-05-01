cb1fac922cc5200044fcfe513a7933679e438fba
tako_desktop <takotabak@gmail.com>
2019-05-01 09:58:11 +0200

    elseif instead of switch

--- screens/MetaQuestionScreen.js
+++ screens/MetaQuestionScreen.js
@@ -12,10 +12,10 @@ import {
 } from 'react-native';
 
 import { QuestionsScreen } from './QuestionsScreen';
-import * as q1 from '../questionData/questions1.json';
-import * as q2 from '../questionData/questions2.json';
-import * as q3 from '../questionData/questions3.json';
-import * as q4 from '../questionData/questions4.json';
+import q1 from '../questionData/questions1.json';
+import q2 from '../questionData/questions2.json';
+import q3 from '../questionData/questions3.json';
+import q4 from '../questionData/questions4.json';
 
 export default class MetaQuestionsScreen extends React.Component {
     state = {
@@ -29,37 +29,45 @@ export default class MetaQuestionsScreen extends React.Component {
         console.log('meta' + current_questions)
         console.log(q2)
         console.log('---meta---q2')
-        switch (current_questions) {
-            case (1):
-                console.log("Metaswitch " + current_questions);
-                this.props.navigation.navigate('Questions', {
-                    questions: { q1 },
-                    question_meta_num: current_questions
+        if (current_questions == 1) {
+            console.log("Metaswitch " + current_questions);
+            this.props.navigation.navigate('Questions', {
+                questions: { q1 },
+                question_meta_num: current_questions
 
-                })
-            case (2):
-                console.log("Metaswitch " + current_questions);
-                this.props.navigation.navigate('Questions', {
-                    questions: q2,
-                    question_meta_num: current_questions
+            })
+        }
+        else if (current_questions == 2) {
 
-                })
-            case (3):
-                console.log("Metaswitch " + current_questions);
-                this.props.navigation.navigate('Questions', {
-                    questions: { q3 },
-                    question_meta_num: current_questions
+            console.log("Metaswitch " + current_questions);
+            this.props.navigation.navigate('Questions', {
+                questions: q2,
+                question_meta_num: current_questions
 
-                })
-            case (4):
-                console.log("Metaswitch " + current_questions);
-                this.props.navigation.navigate('Questions', {
-                    questions: { q4 },
-                    question_meta_num: current_questions
+            })
+        }
+        else if (current_questions == 3) {
 
-                })
-            case (5):
-                this.props.navigation.navigate('Home')
+            console.log("Metaswitch " + current_questions);
+            this.props.navigation.navigate('Questions', {
+                questions: { q3 },
+                question_meta_num: current_questions
+
+            })
+        }
+
+        else if (current_questions == 4) {
+            console.log("Metaswitch " + current_questions);
+            this.props.navigation.navigate('Questions', {
+                questions: { q4 },
+                question_meta_num: current_questions
+
+            })
+        }
+        else {
+            console.log("Metaswitch hard " + 5);
+            console.log("Metaswitch " + current_questions);
+            this.props.navigation.navigate('Home')
         }
         return <View><Text>Loading..</Text></View>
     };
