0c9b4dd21aac8b411db0ba7d0ef3b5270ed1b7fd
tako_desktop <takotabak@gmail.com>
2019-05-13 14:57:12 +0200

    :sparkles: login screen progress

--- screens/LoginFirebase.js
+++ screens/LoginFirebase.js
@@ -1,15 +1,40 @@
-import firebase from '../firebase-keys'
+import React from 'react';
+import View from 'react-native';
+
+import * as firebase from 'firebase';
+
 import FirebaseLogin from "./FirebaseLogin";
 
+firebasekeys = require('../firebase-keys.json')
+// Initialize Firebase
+const firebaseConfig = {
+    apiKey: firebasekeys["apikey"],
+    authDomain: firebasekeys["authDomain"],
+    databaseURL: firebasekeys["databaseURL"],
+    storageBucket: firebasekeys["storageBucket"]
+};
+
+firebase.initializeApp(firebaseConfig);
 
 export default class LoginFirebase extends React.Component {
 
+    login = (user) => {
+        console.log('user loged in!!!')
+        console.log(user)
+        this.props.navigation.navigate('Home', {
+            user: user,
+        })
+    }
 
     render() {
         return (
-            <FirebaseLogin
-                login={user => console.warn(user)}
-            />
+            <View>
+                <FirebaseLogin
+                    login={(user) => {
+                        this.login(user)
+                    }}
+                />
+            </View >
         )
     }
 }
