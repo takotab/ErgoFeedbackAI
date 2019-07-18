c1b0a63bd70ac0c5bfdadff126ef3f0bf7cc9f38
tako_desktop <takotabak@gmail.com>
2019-07-18 09:59:47 +0200

    removed default this seems to cause the show problem

--- components/CheckAno.js
+++ components/CheckAno.js
@@ -21,69 +21,38 @@ import { Button } from "react-native-elements";
 import { UploadDctAsync } from "./uploadJson";
 import { w } from "./Dimensions";
 
-export default class CheckAno extends React.Component {
-  static navigationOptions = {
-    headerTitleStyle: { alignSelf: "center", fontSize: 20 },
-    headerTitle: "Controleer je foto       "
-  };
+export class CheckAno extends React.Component {
+  constructor() {
+    super();
 
-  state = {
-    // user: this.props.navigation.getParam('user', {}),
-    poseimg:
-      "https://storage.googleapis.com/ergoscan-img/6271f2bc_00b2_46a9_9590_1fac749cb492/photo-0-6271f2bc_00b2_46a9_9590_1fac749cb492_w_pose_bbox.png"
-  };
-
-  download = (url, name) => {
-    const { config, fs } = RNFetchBlob;
-    let PictureDir = fs.dirs.PictureDir;
-    let options = {
-      fileCache: true,
-      addAndroidDownloads: {
-        useDownloadManager: true,
-        notification: true,
-        path: PictureDir + name,
-        description: t("downloading_file")
-      }
+    this.state = {
+      poseimg:
+        "https://storage.googleapis.com/ergoscan-img/6271f2bc_00b2_46a9_9590_1fac749cb492/photo-0-6271f2bc_00b2_46a9_9590_1fac749cb492_w_pose_bbox.png"
     };
-    config(options)
-      .fetch("GET", url)
-      .then(res => {
-        if (res.data) {
-          alert(t("download_success"));
-        } else {
-          alert(t("download_failed"));
-        }
-      });
-  };
+  }
+
   _goNext = async () => {
     await console.log("goNext");
 
     response = await UploadDctAsync(
       {
-        ImageCheck: this.state.poseimg,
+        ImageCheck: this.props.poseimg,
         humanCheck: "True"
       },
       "to_firebase"
     );
-    this.props.navigation.navigate("Questions");
+    this.props.goNext();
   };
 
   _goback = async () => {
     response = await UploadDctAsync(
       {
-        ImageCheck: this.state.poseimg,
+        ImageCheck: this.props.poseimg,
         humanCheck: "False"
       },
       "to_firebase"
     );
-  };
-
-  _restore = () => {
-    this.setState({
-      hasPhotos: false,
-      uri: "",
-      wait: false
-    });
+    this.props.restore();
   };
 
   renderbutton = (name, onpress) => {
@@ -108,76 +77,77 @@ export default class CheckAno extends React.Component {
       );
     }
   };
-  onSelect = async (key, answer) => {
-    console.log("selected " + answer + " for " + key);
-    this.setState({
-      answer: answer
-    });
-  };
   render() {
-    console.log(this.state.poseimg);
+    console.log("checkanno");
+    console.log(this.props);
+    console.log(this.props.poseimg);
 
     return (
       <View>
-        <View
-          style={{
-            padding: 25,
-            backgroundColor: "white",
-            margin: 5
-          }}
-        />
-        <View
-          style={{
-            justifyContent: "center"
-          }}
-        >
-          <Text style={styles.text}>Kloppen de anotaties in de foto?</Text>
-
-          <Image
-            source={{ uri: this.state.poseimg }}
-            style={{ width: 300, height: 400 }}
-          />
-          <Text style={styles.text}>
-            Let aub op de locatie van de volgende markers:
-          </Text>
-          <View style={{ padding: 25 }}>
-            <FlatList
-              data={[
-                { key: "Oor" },
-                { key: "Schouder" },
-                { key: "Elleboog" },
-                { key: "Pols" },
-                { key: "Heup" },
-                { key: "Knie" },
-                { key: "Enkel" }
-              ]}
-              renderItem={({ item }) => <Text>- {item.key}</Text>}
-            />
-          </View>
-          <View
-            style={{
-              // padding: 15,
-              backgroundColor: "white",
-              margin: 5
-            }}
-          />
-          {this.renderbutton("ja", this._goNext)}
-          <View
-            style={{
-              padding: 10,
-              backgroundColor: "white",
-              margin: 5
-            }}
-          />
-          <Button
-            key="nope"
-            style={styles.button}
-            onPress={this._restore}
-            title="Nee"
-          />
-        </View>
+        <Text>Hello</Text>
       </View>
     );
+    // (
+    //   <View>
+    //     <View
+    //       style={{
+    //         padding: 25,
+    //         backgroundColor: "white",
+    //         margin: 5
+    //       }}
+    //     />
+    //     <View
+    //       style={{
+    //         justifyContent: "center"
+    //       }}
+    //     >
+    //       <Text style={styles.text}>Kloppen de anotaties in de foto?</Text>
+
+    //       <Image
+    //         source={{ uri: this.state.poseimg }}
+    //         style={{ width: 300, height: 400 }}
+    //       />
+    //       <Text style={styles.text}>
+    //         Let aub op de locatie van de volgende markers:
+    //       </Text>
+    //       <View style={{ padding: 25 }}>
+    //         <FlatList
+    //           data={[
+    //             { key: "Oor" },
+    //             { key: "Schouder" },
+    //             { key: "Elleboog" },
+    //             { key: "Pols" },
+    //             { key: "Heup" },
+    //             { key: "Knie" },
+    //             { key: "Enkel" }
+    //           ]}
+    //           renderItem={({ item }) => <Text>- {item.key}</Text>}
+    //         />
+    //       </View>
+    //       <View
+    //         style={{
+    //           // padding: 15,
+    //           backgroundColor: "white",
+    //           margin: 5
+    //         }}
+    //       />
+    //       {this.renderbutton("ja", this._goNext)}
+    //       <View
+    //         style={{
+    //           padding: 10,
+    //           backgroundColor: "white",
+    //           margin: 5
+    //         }}
+    //       />
+    //       <Button
+    //         key="nope"
+    //         style={styles.button}
+    //         onPress={this._restore}
+    //         title="Nee"
+    //       />
+    //     </View>
+    //   </View>
+    // );
   }
 }
 const styles = StyleSheet.create({
